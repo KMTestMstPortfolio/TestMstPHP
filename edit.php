52a4967957b26de2d252226b031750532404cbcb
km <work-km-books@hotmail.co.jp>
2022-07-12 00:01:01 +0900

    画面修正

--- edit.php
+++ edit.php
@@ -45,22 +45,47 @@
 	<div class="contents">
 		<form method="post" method="post">
 			<input type="hidden" name="kanri_no" value="<?php echo isset($_POST['edit']) ? $row['kanri_no'] : ''; ?>">
-			<p><label>作業者ID：</label><input type="text" name="user_id" value="<?php echo isset($_POST['edit']) ? $row['user_id'] : ''; ?>" maxlength='30'>
-			<p><label>作業者名：</label><input type="text" name="user_nm" value="<?php echo isset($_POST['edit']) ? $row['user_nm'] : ''; ?>"  maxlength='30'>
-			<p><label>部門：</label>
-			<select name="bumon_cd" class="form-control" id="bumon_Cd">
+			<table class="edit_table">
+			<tr>
+	            <td colspan="2">以下の内容を入力してください。</td>
+	        </tr>
+			<tr>
+	            <td>作業者ID</td>
+				<td><input type="text" name="user_id" value="<?php echo isset($_POST['edit']) ? $row['user_id'] : ''; ?>" maxlength='30'></td>
+			</tr>
+			<tr>
+				<td>作業者名</td>
+				<td><input type="text" name="user_nm" value="<?php echo isset($_POST['edit']) ? $row['user_nm'] : ''; ?>"  maxlength='30'></td>
+			</tr>
+			<tr>
+				<td>部門</td>
+				<td><select name="bumon_cd" class="form-control" id="bumon_Cd">
 				<option value="0" <?php echo empty($row['bumon_cd']) ? 'selected' : ''; ?>>選択しない</option>
 				<option value="A01" <?php echo isset($row['bumon_cd']) && $row['bumon_cd'] == 'A01' ? 'selected' : '' ?>>製造部門</option>
 				<option value="A02" <?php echo isset($row['bumon_cd']) && $row['bumon_cd'] == 'A02' ? 'selected' : '' ?>>管理部門</option>
 				<option value="A03" <?php echo isset($row['bumon_cd']) && $row['bumon_cd'] == 'A03' ? 'selected' : '' ?>>技術部門</option>
-			</select>
-			<p><label>作業場：</label><input type="text" name="sagyoba_mei" value="<?php echo isset($_POST['edit']) ? $row['sagyoba_mei'] : ''; ?>" maxlength='5'></p>
-			<p><label>メールアドレス：</label><input type="text" name="address" value="<?php echo isset($_POST['edit']) ? $row['address'] : ''; ?>" maxlength='30'></p>
-			<p><label>携帯番号：</label><input type="text" name="phone" value="<?php echo isset($_POST['edit']) ? $row['phone'] : ''; ?>" maxlength='13'></p>
-			<input type="checkbox" name="muko_flg" <?php echo empty($row['muko_flg']) ? '' : 'checked' ?>>無効フラグ<p>
-			<input type="submit" name="update" value="登録する"> <input type="submit" name="delete" value="削除する">
+				</select><td>
+			</tr>
+			<tr>
+				<td>作業場</td>
+				<td><input type="text" name="sagyoba_mei" value="<?php echo isset($_POST['edit']) ? $row['sagyoba_mei'] : ''; ?>" maxlength='5'></td>
+			</tr>
+				<tr>
+				<td>メールアドレス</td>
+				<td><input type="text" name="address" value="<?php echo isset($_POST['edit']) ? $row['address'] : ''; ?>" maxlength='30'></td>
+			</tr>
+			<tr>
+				<td>携帯番号：</td>
+				<td><input type="text" name="phone" value="<?php echo isset($_POST['edit']) ? $row['phone'] : ''; ?>" maxlength='13'></td>
+			</tr>
+			<tr>
+			<td><input type="checkbox" name="muko_flg" <?php echo empty($row['muko_flg']) ? '' : 'checked' ?>>無効フラグ</td>
+			</tr>
+	        </table>
+			<p><input type="submit" name="update" class="btn_update" value="登録する"> 
+			<input type="submit" class="btn_delete" name="delete" value="削除する"></p>
 		</form>
-		<input type="button" value="戻る" onClick="history.back()">
+		<p><input type="button" value="戻る" class="btn_back" onClick="history.back()"></p>
 	</div>
 	<footer>
     <br>
--- index.php
+++ index.php
@@ -16,71 +16,78 @@ $userData = getMstData($_GET);
 <h1><a href="/index.php">作業者マスタメンテナンス</a></h1>
 <div class="insert">
 	<form method="post" action="edit.php">
-		<input type="submit" name="insert" value="新規投稿">
+		<input type="submit" name="insert" class="btn_insert" value="新規投稿">
 	</form>
 	</div>
 </header>
 <div class="contents">
 <div class="search">
 	<form method="get">
-		<B>検索条件：</B>
-		<div>
-			<label>作業者ID</label>
-			<input name="user_id" value="<?php echo isset($_GET['user_id']) ? htmlspecialchars($_GET['user_id']) : '' ?>">
-			<label>作業者名</label>
-			<input name="user_nm" value="<?php echo isset($_GET['user_nm']) ? htmlspecialchars($_GET['user_nm']) : '' ?>">
-		</div>
-		<div>
-			<label>部門</label>
-			<select name="bumon_cd" id="bumon_Cd">
-				<option value="0" <?php echo empty($_GET['bumon_cd']) ? 'selected' : '' ?>></option>
-				<option value="A01" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A01' ? 'selected' : '' ?>>製造部門</option>
-				<option value="A02" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A02' ? 'selected' : '' ?>>管理部門</option>
-				<option value="A03" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A03' ? 'selected' : '' ?>>技術部門</option>
-			</select>
-			<input type="checkbox" name="muko_flg" <?php echo isset($_GET['muko_flg']) && htmlspecialchars($_GET['muko_flg']) == true  ? 'checked' : '' ?>>
-			<label>無効を含む</label>
-		</div>
-		<button type="submit" name="search">検索</button>
+	<table class="search_table">
+			<tr>
+	            <th>検索条件</th>
+	        </tr>
+	        <tr>
+	            <td>作業者名</td>
+				<td><input name="user_id" value="<?php echo isset($_GET['user_id']) ? htmlspecialchars($_GET['user_id']) : '' ?>"></td>
+				<td>作業者名</td>
+				<td><input name="user_nm" value="<?php echo isset($_GET['user_nm']) ? htmlspecialchars($_GET['user_nm']) : '' ?>"></td>
+	        </tr>
+	        <tr>
+				<td>部門</td>
+	        <td>
+				<select name="bumon_cd" id="bumon_Cd">
+					<option value="0" <?php echo empty($_GET['bumon_cd']) ? 'selected' : '' ?>></option>
+					<option value="A01" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A01' ? 'selected' : '' ?>>製造部門</option>
+					<option value="A02" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A02' ? 'selected' : '' ?>>管理部門</option>
+					<option value="A03" <?php echo isset($_GET['bumon_cd']) && $_GET['bumon_cd'] == 'A03' ? 'selected' : '' ?>>技術部門</option>
+				</select>
+			</td>
+	        </tr>
+	         <tr>
+				 <td>無効を含む</td>
+				 <td><input type="checkbox" name="muko_flg" <?php echo isset($_GET['muko_flg']) && htmlspecialchars($_GET['muko_flg']) == true  ? 'checked' : '' ?>></td>
+			 </tr>
+	         <tr>
+				 <td><button type="submit" name="search"  class="btn_search" >検索</button></td>
+			 </tr>
+	</table>
 	</form>
 </div>
 <div class="result">
 	<?php if(isset($userData) && count($userData)): ?>
-		<table>
-			<thead>
+		<table class="result_table">
+			<tr>
+				<th>作業者ID</th>
+				<th>作業者名</th>
+				<th>部門</th>
+				<th>作業場</th>
+				<th>メールアドレス</th>
+				<th>携帯番号</th>
+				<th>登録日</th>
+				<th>更新日</th>
+				<th>無効フラグ</th>
+				<th></th>
+			</tr>
+			<?php foreach($userData as $row): ?>
 				<tr>
-					<th>作業者ID</th>
-					<th>作業者名</th>
-					<th>部門</th>
-					<th>作業場</th>
-					<th>メールアドレス</th>
-					<th>携帯番号</th>
-					<th>登録日</th>
-					<th>更新日</th>
-					<th>無効フラグ</th>
+					<td><?php echo htmlspecialchars($row['USER_ID']) ?></td>
+					<td><?php echo htmlspecialchars($row['USER_NM']) ?></td>
+					<td><?php echo htmlspecialchars($row['BUMON_MEI']) ?></td>
+					<td><?php echo htmlspecialchars($row['SAGYOBA_MEI']) ?></td>
+					<td><?php echo htmlspecialchars($row['ADDRESS']) ?></td>
+					<td><?php echo htmlspecialchars($row['PHONE']) ?></td>
+					<td><?php echo date('Y/m/d',  strtotime(htmlspecialchars($row['TOROKUBI']))) ?></td>
+					<td><?php echo date('Y/m/d',  strtotime(htmlspecialchars($row['KOSHINBI']))) ?></td>
+					<td><input type="checkbox" disabled <?php echo htmlspecialchars($row['MUKO_FLG']) == 1 ? 'checked' : ''?>></td>
+					<td>
+						<form method="post" action="edit.php">
+							<input type="submit" name="edit" class="btn_edit" value="編集">
+							<input type="hidden" name="kanri_no" value=<?php echo htmlspecialchars($row['KANRI_NO']) ?>>
+						</form>
+					</td>
 				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($userData as $row): ?>
-					<tr>
-						<td><?php echo htmlspecialchars($row['USER_ID']) ?></td>
-						<td><?php echo htmlspecialchars($row['USER_NM']) ?></td>
-						<td><?php echo htmlspecialchars($row['BUMON_MEI']) ?></td>
-						<td><?php echo htmlspecialchars($row['SAGYOBA_MEI']) ?></td>
-						<td><?php echo htmlspecialchars($row['ADDRESS']) ?></td>
-						<td><?php echo htmlspecialchars($row['PHONE']) ?></td>
-						<td><?php echo date('Y/m/d',  strtotime(htmlspecialchars($row['TOROKUBI']))) ?></td>
-						<td><?php echo date('Y/m/d',  strtotime(htmlspecialchars($row['KOSHINBI']))) ?></td>
-						<td><input type="checkbox" disabled <?php echo htmlspecialchars($row['MUKO_FLG']) == 1 ? 'checked' : ''?>></td>
-						<td>
-							<form method="post" action="edit.php">
-								<input type="submit" name="edit" value="編集">
-								<input type="hidden" name="kanri_no" value=<?php echo htmlspecialchars($row['KANRI_NO']) ?>>
-							</form>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-			</tbody>
+			<?php endforeach; ?>
 		</table>
 	<?php else: ?>
 		<p class="alert alert-danger">検索結果：該当無し</p>
